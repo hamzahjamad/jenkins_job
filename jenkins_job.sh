b6aa8f136260c841809ef538569fe9a96191f1c8
Hamzah Jamad <hamzahjamad@gmail.com>
2016-05-30 17:21:17 +0800

    fixing the tenth argument

--- jenkins_job.sh
+++ jenkins_job.sh
@@ -10,9 +10,10 @@ runscript(){
 	bitbucketusername=$7 
 	bitbucketpassword=$8 
 	bitbucketrepositoryname=$9
-	jenkinprojectname=$10  
+	jenkinprojectname=${10}  
   	sh autocopy_jenkin_jobs/jenkin_copy_job.sh $jenkinusername $jenkinuserapitoken $newjobname $jobnametocopy $jenkinurl $jenkinprojecttoken $bitbucketusername $bitbucketrepositoryname
   	sh autocreate_bitbucket_service/bitbucket_service.sh $bitbucketusername $bitbucketpassword $bitbucketrepositoryname $jenkinusername $jenkinuserapitoken $jenkinurl $jenkinprojecttoken $jenkinprojectname
+
 }
 
 
@@ -22,7 +23,7 @@ runscript(){
 #
 if [ -n "${1}" -a -n "${2}" -a -n "${3}" -a -n "${4}" -a -n "${5}" -a -n "${6}" -a -n "${7}" -a -n "${8}" -a -n "${9}" -a -n "${10}" ]; then
    
-    runscript $1 $2 $3 $4 $5 $6 $7 $8 $9 $10
+    runscript $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10}
 
 else
     echo "Usage:"
