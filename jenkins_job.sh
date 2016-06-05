64ab2e4c51fee882c68698092b779837d4e1454a
Hamzah Jamad <hamzahjamad@gmail.com>
2016-06-05 11:44:15 +0800

    fix bug for bitbucket but github not properly working yet

--- jenkins_job.sh
+++ jenkins_job.sh
@@ -1,35 +1,44 @@
 #!/bin/bash
 
+unittestjob="TaskManager-UnitTest"
+newunittestjob=$unittestjob$6
+
+deployjob="TaskManager-Deploy"
+newdeployjob=$deployjob$6
+
+jenkinsscript(){
+
+	gitprovider=$7
+
+	sh autocopy_jenkin_jobs/jenkin_copy_job.sh $1 $2 $newunittestjob $unittestjob $3 $4 $5 $6 $gitprovider
+	sh autocopy_jenkin_jobs/jenkin_copy_job.sh $1 $2 $newdeployjob $deployjob $3 $4 $5 $6 $gitprovider
+
+}
+
 bitbucketscript(){
 	jenkinusername=$1 
-	jenkinuserapitoken=$2 
-	newjobname=$3 
-	jobnametocopy=$4 
-	jenkinurl=$5 
-	jenkinprojecttoken=$6 
-	bitbucketusername=$7 
-	bitbucketpassword=$8 
-	bitbucketrepositoryname=$9
-	jenkinprojectname=${10} 
+	jenkinuserapitoken=$2  
+	jenkinurl=$3 
+	jenkinprojecttoken=$4 
+	bitbucketusername=$5 
+	bitbucketpassword=$6 
+	bitbucketrepositoryname=$7
 
-
-	sh autocopy_jenkin_jobs/jenkin_copy_job.sh $jenkinusername $jenkinuserapitoken $newjobname $jobnametocopy $jenkinurl $jenkinprojecttoken $bitbucketusername $bitbucketrepositoryname --bitbucket
-  	sh autocreate_bitbucket_service/bitbucket_service.sh $bitbucketusername $bitbucketpassword $bitbucketrepositoryname $jenkinusername $jenkinuserapitoken $jenkinurl $jenkinprojecttoken $jenkinprojectname
+	jenkinsscript $jenkinusername $jenkinuserapitoken $jenkinurl $jenkinprojecttoken $bitbucketusername $bitbucketrepositoryname --bitbucket
+	sh autocreate_bitbucket_service/bitbucket_service.sh $bitbucketusername $bitbucketpassword $bitbucketrepositoryname $jenkinurl $jenkinprojecttoken $newunittestjob
 
 }
 
 githubscript(){
 	jenkinusername=$1 
 	jenkinuserapitoken=$2 
-	newjobname=$3 
-	jobnametocopy=$4 
-	jenkinurl=$5 
-	jenkinprojecttoken=$6 
-	githubusername=$7 
-	githubpassword=$8 
-	githubrepositoryname=$9
+	jenkinurl=$3 
+	jenkinprojecttoken=$4 
+	githubusername=$5 
+	githubpassword=$6 
+	githubrepositoryname=$7
 
-	sh autocopy_jenkin_jobs/jenkin_copy_job.sh $jenkinusername $jenkinuserapitoken $newjobname $jobnametocopy $jenkinurl $jenkinprojecttoken $githubusername $githubrepositoryname --github
+	jenkinsscript $jenkinusername $jenkinuserapitoken $jenkinurl $jenkinprojecttoken $githubusername $githubrepositoryname --github
 	sh autocreate_github_service/github_service.sh $githubusername $githubpassword $githubrepositoryname $jenkinurl
 }
 
@@ -41,20 +50,20 @@ githubscript(){
 
 case ${1} in
     -g|--github)
-   		if [ -n "${1}" -a -n "${2}" -a -n "${3}" -a -n "${4}" -a -n "${5}" -a -n "${6}" -a -n "${7}" -a -n "${8}" -a -n "${9}" -a -n "${10}" ]; then
-        	githubscript $2 $3 $4 $5 $6 $7 $8 $9 ${10}
+   		if [ -n "${1}" -a -n "${2}" -a -n "${3}" -a -n "${4}" -a -n "${5}" -a -n "${6}" -a -n "${7}" -a -n "${8}" ]; then
+        	githubscript $2 $3 $4 $5 $6 $7 $8 
 		else
 		    echo "Usage:"
-		    echo "  Please follow this convention: ${0} 'git_mode = --github|--bitbucket' 'jenkin_username' 'jenkin_user_api_token' 'new_job_name' 'job_name_to_copy' 'jenkin_url' 'jenkin_project_token' 'github_username' 'github_password' 'github_repository_name'"
+		    echo "  Please follow this convention: ${0} --github 'jenkin_username' 'jenkin_user_api_token' 'jenkin_url' 'jenkin_project_token' 'github_username' 'github_password' 'github_repository_name'"
 		    exit 1
 		fi
         ;;
     -b|--bitbucket)
-        if [ -n "${1}" -a -n "${2}" -a -n "${3}" -a -n "${4}" -a -n "${5}" -a -n "${6}" -a -n "${7}" -a -n "${8}" -a -n "${9}" -a -n "${10}" -a -n "${11}" ]; then
-    		bitbucketscript $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11}
+        if [ -n "${1}" -a -n "${2}" -a -n "${3}" -a -n "${4}" -a -n "${5}" -a -n "${6}" -a -n "${7}" -a -n "${8}" ]; then
+    		bitbucketscript $2 $3 $4 $5 $6 $7 $8
 		else
 		    echo "Usage:"
-		    echo "  Please follow this convention: ${0} 'git_mode = --github|--bitbucket' 'jenkin_username' 'jenkin_user_api_token' 'new_job_name' 'job_name_to_copy' 'jenkin_url' 'jenkin_project_token' 'bitbucket_username' 'bitbucket_password' 'bitbucket_repository_name' 'jenkin_project_name' "
+		    echo "  Please follow this convention: ${0} --bitbucket 'jenkin_username' 'jenkin_user_api_token' 'jenkin_url' 'jenkin_project_token' 'bitbucket_username' 'bitbucket_password' 'bitbucket_repository_name' "
 		    exit 1
 		fi
         ;;
